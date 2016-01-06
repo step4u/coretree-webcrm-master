852ab1e01e5f6f4cc6e125bd862ec7ca291a7a37
sequel2 <sequel2@RD0>
2016-01-07 01:32:06 +0900

    2016-01-07 #1
    Signed-off-by: sequel2 <sequel2@RD0>

--- src/main/java/com/coretree/defaultconfig/config/WebSecurityConfig.java
+++ src/main/java/com/coretree/defaultconfig/config/WebSecurityConfig.java
@@ -1,6 +1,8 @@
 
 package com.coretree.defaultconfig.config;
 
+import javax.sql.DataSource;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -16,8 +18,9 @@ import org.springframework.security.web.header.writers.frameoptions.XFrameOption
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Autowired
-	LoginHandler loginSuccessHandler;
-	// private LogoutHandler logoutSuccessHandler = new LogoutHandler();
+	private LoginHandler loginSuccessHandler;
+	@Autowired
+	private DataSource dataSrc;
 
 	@Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -51,9 +54,21 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 				.and();
     }
 	
-	@Autowired
+/*	@Autowired
 	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-		auth.inMemoryAuthentication().withUser("test").password("1234").roles("USER", "ADMIN");
-		auth.inMemoryAuthentication().withUser("test1").password("1234").roles("USER");
+		//auth.inMemoryAuthentication().withUser("test").password("1234").roles("USER", "ADMIN");
+		//auth.inMemoryAuthentication().withUser("test1").password("1234").roles("USER");
+	}*/
+	
+	@Autowired
+	public void configAuthentication(AuthenticationManagerBuilder auth) throws Exception {
+		
+		String getUser = "select a.username, a.PASSWORD, a.enabled from users a join user_roles b on a.idx=b.user_idx where username=?";
+		String getAuth = "select username, role from users a join user_roles b on a.idx=b.user_idx where a.username=?";
+		
+		auth.jdbcAuthentication()
+			.dataSource(dataSrc)
+			.usersByUsernameQuery(getUser)
+			.authoritiesByUsernameQuery(getAuth);
 	}
 }
