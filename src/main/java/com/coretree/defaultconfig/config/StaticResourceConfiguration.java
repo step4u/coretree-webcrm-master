6fddd43e0ec9ea7926828c699a4b300e38e74183
JC Lee <step4u@gmail.com>
2016-01-26 18:59:58 +0900

    2016-01-26 18:59
    Signed-off-by: JC Lee <step4u@gmail.com>

--- src/main/java/com/coretree/defaultconfig/config/StaticResourceConfiguration.java
+++ src/main/java/com/coretree/defaultconfig/config/StaticResourceConfiguration.java
@@ -11,7 +11,8 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 public class StaticResourceConfiguration extends WebMvcConfigurerAdapter {
 	private static final Logger LOG = LoggerFactory.getLogger(StaticResourceConfiguration.class);
 	// private String staticPath = "file:///D:/spring_boot_test/";
-	private String staticPath = "file:///D:/dev/test/Java/";
+	//private String staticPath = "file:///D:/dev/test/Java/";
+	private String staticPath = "/home/webcrm/media/";
 	
 	@Override
 	 public void addResourceHandlers(ResourceHandlerRegistry registry) {
