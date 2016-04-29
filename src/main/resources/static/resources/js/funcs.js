50e589caf084555c2bf294c0cee347621ba50f1c
JC LEE <step4u@gmail.com>
2016-04-29 15:46:52 +0900

    2016-04-29 15:46
    Signed-off-by: JC LEE <step4u@gmail.com>

--- src/main/resources/static/resources/js/funcs.js
+++ src/main/resources/static/resources/js/funcs.js
@@ -83,53 +83,6 @@
 				}
 				
 				$("#ModalSms").modal("hide");
-
-/*				
-			    var smsinfo = {
-					idx: -1,
-					custs_idx: '',
-					custs_tel: $("#receivephones").val(),
-					contents: $("#smstxt").val(),
-					result: item.status,
-					regdate: '',
-					sdate: '',
-					edate: '',
-					curpage: '',
-					rowsperpage: ''
-			    };
-
-			    //$("#ModalSms").modal("hide");
-			    
-			    $.ajax({
-			    	type: "POST",
-			    	url: "/sms/add/msg",
-			    	data: smsinfo,
-			    	dataType: "json",
-			    	success: function(data){
-						$("#ModalSms").modal("hide");
-						
-						if ($("#ctrlsms").length) {
-					    	var scope = angular.element($("#ctrlsms")).scope();
-						    scope.$apply(function () {
-						        scope.getPage();
-						    });
-						}
-			    	},
-			    	error: function(data){
-			    		
-			    	}
-			    });
-*/
-/*			    $.post("/sms/add/msg", smsinfo, function(response){
-					$("#ModalSms").modal("hide");
-					
-					if ($("#ctrlsms").length) {
-				    	var scope = angular.element($("#ctrlsms")).scope();
-					    scope.$apply(function () {
-					        scope.getPage();
-					    });
-					}
-			    });*/
 				break;
 			case WS_RES_EXTENSION_STATE:
 				update_ext_status(item);
